d521142237116633b432312a01253cb0e7a28e39
jorgegarve <jorgegarve@gmail.com>
2019-01-13 17:42:56 +0100

    Added input handler

---
+++ vs-production/Assets/Scripts/Command.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CommandPattern
+{	
+	
+	public abstract class Command
+	{
+		public abstract void Execute (Transform objectTransform);
+	}
+
+	public class MoveForward : Command
+	{
+		public override void Execute (Transform objectTransform)
+		{
+			Debug.Log ("Doing MoveForwards");
+		}
+	}
+
+	public class MoveBackward : Command
+	{
+		public override void Execute (Transform objectTransform)
+		{
+			Debug.Log ("Doing MoveBackwards");
+		}
+	}
+
+	public class MoveLeft : Command
+	{
+		public override void Execute (Transform objectTransform)
+		{
+			Debug.Log ("Doing MoveLeft");
+		}
+	}
+
+	public class MoveRight : Command
+	{
+		public override void Execute (Transform objectTransform)
+		{
+			Debug.Log ("Doing MoveRight");
+		}
+	}
+
+	public class SelectAttackTarget : Command
+	{
+		public override void Execute (Transform objectTransform)
+		{
+			Debug.Log ("Doing SelectAttackTarget");
+		}
+	}
+
+    public class Cancel : Command
+    {
+        public override void Execute(Transform objectTransform)
+        {
+			Debug.Log ("Doing Cancel");
+        }
+    }
+
+}
---
+++ vs-production/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CommandPattern
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public Transform player;
+        private Command buttonW, buttonS, buttonA, buttonD, buttonRMB, buttonESC;
+
+        void Start ()
+        {
+            buttonW = new MoveForward();
+            buttonS = new MoveBackward();
+            buttonA = new MoveLeft();
+            buttonD = new MoveRight();
+            buttonRMB = new SelectAttackTarget();
+            buttonESC = new Cancel();
+        }
+
+        void Update()
+        {
+            HandleInput();
+        }
+
+        public void HandleInput ()
+        {
+            if (Input.GetKeyDown (KeyCode.A))
+            {
+                buttonA.Execute (player);
+            }
+            else if (Input.GetKeyDown (KeyCode.D))
+            {
+                buttonD.Execute (player);
+            }
+            else if (Input.GetKeyDown (KeyCode.S))
+            {
+                buttonS.Execute (player);
+            }
+            else if (Input.GetKeyDown (KeyCode.W))
+            {
+                buttonW.Execute (player);
+            }
+            else if (Input.GetMouseButtonDown (1))
+            {
+                buttonRMB.Execute (player);
+            }
+            else if (Input.GetKeyDown (KeyCode.Escape))
+            {
+                buttonESC.Execute (player);
+            }
+        }
+    }
+}
