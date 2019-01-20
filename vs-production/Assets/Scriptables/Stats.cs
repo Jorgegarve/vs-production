82970411befd7b8de6670c64ec54d09f59362c6d
jorgegarve <jorgegarve@gmail.com>
2019-01-20 18:10:19 +0100

    Added enemy detection

---
+++ vs-production/Assets/Scriptables/Stats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Health {
+	public int maxHealth;
+	public int healthRegeneration;
+	public float healthRegTime;
+
+    public Health ()
+    {
+
+    }
+}
+
+[CreateAssetMenu(fileName = "New Stats", menuName = "Stats", order = 51)]
+public class Stats : ScriptableObject
+{
+    [Header("Health")]
+    public Health health = new Health ();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- vs-production/Assets/Scripts/CombatSystemManager.cs
+++ vs-production/Assets/Scripts/CombatSystemManager.cs
@@ -5,7 +5,10 @@ using DG.Tweening;
 
 public class CombatSystemManager : MonoBehaviour
 {
+    public List<GameObject> reachableEnemies;
+
     public GameObject rangeSpherePrefab;
+    public float rangeSphereRadius = 5f;
     public bool isRangeSphereEnabled = false;
     private GameObject rangeSphere;
 
@@ -46,13 +49,35 @@ public class CombatSystemManager : MonoBehaviour
     {
         isRangeSphereEnabled = true;
         rangeSphere = Instantiate (rangeSpherePrefab, GameManager.Instance.player.position, Quaternion.identity);
-        rangeSphere.transform.DOScale (new Vector3(5, 5, 5), 0.5f).SetUpdate (true).OnComplete ( () => {
-
+        rangeSphere.transform.DOScale (new Vector3(rangeSphereRadius, rangeSphereRadius, rangeSphereRadius), 0.5f).SetUpdate (true).OnComplete ( () => {
+            
         });
+        DetectEnemiesInsideSphere ();
+    }
+
+    private void DetectEnemiesInsideSphere ()
+    {
+        int layerMask = 1 << 10;
+        Collider[] hitColliders = Physics.OverlapSphere (this.transform.position, rangeSphereRadius, layerMask);
+
+        int i = 0;
+        foreach (Collider hit in hitColliders) 
+        {
+            reachableEnemies.Add (hit.gameObject);
+            i++;
+        }
+
+        Debug.Log (i + " enemies detected!");
+    }
+
+    private void ClearReachableEnemiesList ()
+    {
+        reachableEnemies.Clear ();
     }
 
     public void DestroyRangeSphere ()
     {
+        ClearReachableEnemiesList ();
         isRangeSphereEnabled = false;
         Destroy (rangeSphere);
     }
